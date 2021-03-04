fbc33c6ea3543101c23d64c1d13f85e06a5d1d89
sidharrth <sidharrthnix@gmail.com>
2021-03-04 20:01:17 +0000

    lol

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import Header from './components/header/Header';
 import store from './redux/store';
 function App() {
   return (
     <Provider store={store}>
+      <Header />
       <div className="app">Redux</div>
     </Provider>
   );
